58ca8dfb18e909d9b6cc52750929a0407df65b08
Benzema9Real <footballfceasports>
2024-07-09 17:56:09 +0600

    add TunkStackQuery

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <ChakraProvider> 
-
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
     </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
